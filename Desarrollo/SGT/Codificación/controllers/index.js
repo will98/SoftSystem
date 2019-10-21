10e4bd583bfd2c8b4040d3fa630970caf4149b5d
JhordanVega <jhordan.vega@unmsm.edu.pe>
2019-10-20 20:14:27 -0500

    Agregar home

--- Desarrollo/SGT/Codificación/controllers/index.js
+++ Desarrollo/SGT/Codificación/controllers/index.js
@@ -1,5 +1,11 @@
+
 exports.test = (req, res, next) => {
     res.render('test', {title: "Página de inicio", layout: "main"});
+}
+
+
+exports.home = (req, res, next) => {
+    res.render('home/home', {title: "Home", layout: "main"});
 }
 
 exports.comunidad_coleccion = (req, res, next) => {
--- Desarrollo/SGT/Codificación/routes/index.js
+++ Desarrollo/SGT/Codificación/routes/index.js
@@ -5,4 +5,6 @@ const controller = require('../controllers')
 Router.get("/", controller.test)
 Router.get("/comunidad_coleccion", controller.comunidad_coleccion)
 
+Router.get("/home", controller.home)
+
 module.exports = Router

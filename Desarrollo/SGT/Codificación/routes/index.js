93555cea59962d6f0f55d8a70fbe0d0d4b5034bc
WilldV <williant.marquez@gmail.com>
2019-10-28 00:21:50 -0500

    Controladores de nicio de sesión y registro agregados

--- Desarrollo/SGT/Codificación/routes/index.js
+++ Desarrollo/SGT/Codificación/routes/index.js
@@ -1,7 +1,9 @@
 const Router = require('express').Router()
 const controller = require('../controllers')
-
+const acceso = require('../routes/acceso')
 
 Router.get("/", controller.test)
+Router.use("/acceso", acceso)
+
 
 module.exports = Router

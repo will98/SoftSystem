7c7c12881bb2e48816029fc36468877008e82770
WilldV <williant.marquez@gmail.com>
2019-11-02 01:20:06 -0500

    Agregado middleware para subir archivos

--- Desarrollo/SGT/Codificación/routes/tesis.js
+++ Desarrollo/SGT/Codificación/routes/tesis.js
@@ -1,6 +1,8 @@
 const Router = require('express').Router()
 const tesis = require('../controllers/tesis')
 
-Router.post("/crear", tesis.crear)
+const s3upload = require('../middlewares/s3-upload')
+
+Router.post("/crear", s3upload.single("archivo"), tesis.crear)
 
 module.exports = Router

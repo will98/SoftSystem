a0967d4e27b6c91176e88afad84092c7f5d318ee
WilldV <williant.marquez@gmail.com>
2019-11-02 22:47:18 -0500

    Agregados buscadores

--- Desarrollo/SGT/Codificación/controllers/buscador.js
+++ Desarrollo/SGT/Codificación/controllers/buscador.js
@@ -1,5 +1,4 @@
 const Documento = require('../models/Documento')
-const TipoDocumento = require('../models/TipoDocumento')
 
 const AppError = require("../helpers/AppError")
 
@@ -21,7 +20,7 @@ exports.buscar = async (req, res, next) => {
         }
 
         if (query.titulo && query.titulo != '') {
-            filter.titulo = { $regex: new RegExp(`${query.titulo}+?`), $options: 'i' } 
+            filter.titulo = { $regex: new RegExp(`${query.titulo}+?`), $options: 'i' }
         }
 
         const count = await Documento.countDocuments(filter)
@@ -39,17 +38,53 @@ exports.buscar = async (req, res, next) => {
     } catch (error) {
         next(new AppError(error))
     }
-   
+
 };
 
 
 exports.asesor = async (req, res, next) => {
     try {
-        throw new Error("No soportado")
+        const query = req.query
+        const filter = {
+            tipo_documento: 1
+        }
+        let page = 1, limit = 10
+
+        if (query.page && query.page != '') {
+            page = parseInt(query.page)
+        }
+
+        if (query.limit && query.limit != '') {
+            limit = parseInt(query.limit)
+        }
+
+        if (query.asesor && query.asesor != '') {
+            filter.asesores = {
+                $elemMatch: {
+                    $regex: new RegExp(`${query.asesor}+?`), 
+                    $options: 'i'
+                }
+            }
+        }
+
+        const count = await Documento.countDocuments(filter)
+        const pages = Math.ceil(count / limit)
+        const skip = limit * (page - 1)
+        
+        const documentos = await Documento.find(filter).skip(skip).limit(limit).sort({ "fecha": -1 })
+
+        res.status(200).send({
+            limit,
+            count,
+            page,
+            pages,
+            documentos
+        })
     } catch (error) {
         next(new AppError(error))
     }
 }
+
 
 exports.comunidad_coleccion = async (req, res, next) => {
     try {
@@ -59,6 +94,7 @@ exports.comunidad_coleccion = async (req, res, next) => {
     }
 }
 
+
 exports.facultad = async (req, res, next) => {
     try {
         throw new Error("No soportado")
@@ -67,21 +103,89 @@ exports.facultad = async (req, res, next) => {
     }
 }
 
+
 exports.grado_academico = async (req, res, next) => {
     try {
-        throw new Error("No soportado")
+        const query = req.query
+        const filter = {
+            tipo_documento: 1
+        }
+        let page = 1, limit = 10
+
+        if (query.page && query.page != '') {
+            page = parseInt(query.page)
+        }
+
+        if (query.limit && query.limit != '') {
+            limit = parseInt(query.limit)
+        }
+
+        if (query.grado && query.grado != '') {
+            filter.grado = query.grado
+        }
+
+        const count = await Documento.countDocuments(filter)
+        const pages = Math.ceil(count / limit)
+        const skip = limit * (page - 1)
+        
+        const documentos = await Documento.find(filter).skip(skip).limit(limit).sort({ "fecha": -1 })
+
+        res.status(200).send({
+            limit,
+            count,
+            page,
+            pages,
+            documentos
+        })
     } catch (error) {
         next(new AppError(error))
     }
 }
 
+
 exports.palabra_clave = async (req, res, next) => {
     try {
-        throw new Error("No soportado")
+        const query = req.query
+        const filter = {
+            tipo_documento: 1
+        }
+        let page = 1, limit = 10
+
+        if (query.page && query.page != '') {
+            page = parseInt(query.page)
+        }
+
+        if (query.limit && query.limit != '') {
+            limit = parseInt(query.limit)
+        }
+
+        if (query.clave && query.clave != '') {
+            filter.palabras_clave = {
+                $elemMatch: {
+                    $regex: new RegExp(`${query.clave}+?`), 
+                    $options: 'i'
+                }
+            }
+        }
+
+        const count = await Documento.countDocuments(filter)
+        const pages = Math.ceil(count / limit)
+        const skip = limit * (page - 1)
+        
+        const documentos = await Documento.find(filter).skip(skip).limit(limit).sort({ "fecha": -1 })
+
+        res.status(200).send({
+            limit,
+            count,
+            page,
+            pages,
+            documentos
+        })
     } catch (error) {
         next(new AppError(error))
     }
 }
+
 
 exports.buqueda_avanzada = async (req, res, next) => {
     try {
@@ -91,17 +195,87 @@ exports.buqueda_avanzada = async (req, res, next) => {
     }
 }
 
+
 exports.mas_visitadas = async (req, res, next) => {
     try {
-        throw new Error("No soportado")
+        const query = req.query
+        const filter = {
+            tipo_documento: 1
+        }
+        let page = 1, limit = 10
+
+        if (query.page && query.page != '') {
+            page = parseInt(query.page)
+        }
+
+        if (query.limit && query.limit != '') {
+            limit = parseInt(query.limit)
+        }
+
+        if (query.min && query.min != '') {
+            filter.numero_vistas = { $gte: parseInt(query.min)}
+        }
+
+        if (query.max && query.max != '') {
+            filter.numero_vistas = { $lte: parseInt(query.max)}
+        }
+
+        const count = await Documento.countDocuments(filter)
+        const pages = Math.ceil(count / limit)
+        const skip = limit * (page - 1)
+        
+        const documentos = await Documento.find(filter).skip(skip).limit(limit).sort({ "numero_vistas": -1, "fecha": -1 })
+
+        res.status(200).send({
+            limit,
+            count,
+            page,
+            pages,
+            documentos
+        })
     } catch (error) {
         next(new AppError(error))
     }
 }
 
+
 exports.rango_anios = async (req, res, next) => {
     try {
-        throw new Error("No soportado")
+        const query = req.query
+        const filter = {
+            tipo_documento: 1
+        }
+        let page = 1, limit = 10
+
+        if (query.page && query.page != '') {
+            page = parseInt(query.page)
+        }
+
+        if (query.limit && query.limit != '') {
+            limit = parseInt(query.limit)
+        }
+
+        if (query.min && query.min != '') {
+            filter.fecha = { $gte: parseInt(query.min)}
+        }
+
+        if (query.max && query.max != '') {
+            filter.fecha = { $lte: parseInt(query.max)}
+        }
+
+        const count = await Documento.countDocuments(filter)
+        const pages = Math.ceil(count / limit)
+        const skip = limit * (page - 1)
+        
+        const documentos = await Documento.find(filter).skip(skip).limit(limit).sort({ "fecha": -1 })
+
+        res.status(200).send({
+            limit,
+            count,
+            page,
+            pages,
+            documentos
+        })
     } catch (error) {
         next(new AppError(error))
     }

4ff396e4294912bc90e2ecee7e0a7208a6fcd974
DiegoYance <diego.yance@unmsm.edu.pe>
2019-12-08 18:52:09 -0500

    Preparacion de controladores para el usuario

--- Desarrollo/SGT/Codificación/controllers/usuario.js
+++ Desarrollo/SGT/Codificación/controllers/usuario.js
@@ -1,3 +1,23 @@
 exports.home = (req, res, next) => {
     res.render('usuario/principal', {title: "Home del usuario", layout: "mainUsuario"});
 }
+
+exports.getEditar = (req, res, next) => {
+    res.render('usuario/editarPerfil', {title: "Editar usuario", layout: "mainUsuario"});
+}
+
+exports.postEditar = (req, res, next) => {
+    res.render('usuario/editarPerfil', {title: "Editar usuario", layout: "mainUsuario"});
+}
+
+exports.getTesisFavorita = (req, res, next) => {
+    res.render('usuario/tesisFavorita', {title: "Tesis", layout: "mainUsuario"});
+}
+
+exports.getTesisRecomendado = (req, res, next) => {
+    res.render('usuario/tesisRecomendado', {title: "Tesis", layout: "mainUsuario"});
+}
+
+exports.getVisualizarNoticias = (req, res, next) => {
+    res.render('usuario/visualizarNoticias', {title: "Noticias", layout: "mainUsuario"});
+}
--- Desarrollo/SGT/Codificación/routes/usuario.js
+++ Desarrollo/SGT/Codificación/routes/usuario.js
@@ -2,5 +2,10 @@ const Router = require('express').Router()
 const controller = require('../controllers/usuario')
 
 Router.get("/", controller.home)
+Router.get("/editar", controller.getEditar)
+Router.post("/editar", controller.getEditar)
+Router.get("/favorito", controller.getTesisFavorita)
+Router.get("/recomendado", controller.getTesisRecomendado)
+Router.get("/noticias", controller.getVisualizarNoticias)
 
 module.exports = Router

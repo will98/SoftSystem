98e559e9e80c25678bb3313505ea644aa6ba9485
WilldV <williant.marquez@gmail.com>
2019-12-08 21:32:18 -0500

    Agregado logout

--- Desarrollo/SGT/Codificación/controllers/acceso.js
+++ Desarrollo/SGT/Codificación/controllers/acceso.js
@@ -6,30 +6,30 @@ const Usuario = require('../models/Usuario')
 const TipoUsuario = require('../models/TipoUsuario')
 
 exports.getLogin = async (req, res, next) => {
-    res.render('acceso/loginUsuario', { title: "Acceso", layout: "main" });
+  res.render('acceso/loginUsuario', { title: "Acceso", layout: "main" });
 }
 
 exports.postLogin = (req, res, next) => {
-    passport.authenticate('local-login', async (err, user, info) => {
-        try {
-          if (err) {
-            return next(new AppError(error, `/acceso`));
-          }
-          if (!user) {
-            return next(new AppError(new Error(info.msg), `/acceso`));
-          }
-          req.logIn(user, (err) => {
-            if (err) { return next(new AppError(err, `/acceso`));}
-            if (user.tipoUsuario == 2){
-              return res.redirect('/usuario');
-            }else{
-              return res.redirect('/homeadmin');
-            }
-          });
-        } catch (error) {
-          return next(new AppError(error, `/acceso`));
+  passport.authenticate('local-login', async (err, user, info) => {
+    try {
+      if (err) {
+        return next(new AppError(error, `/acceso`));
+      }
+      if (!user) {
+        return next(new AppError(new Error(info.msg), `/acceso`));
+      }
+      req.logIn(user, (err) => {
+        if (err) { return next(new AppError(err, `/acceso`)); }
+        if (user.tipoUsuario == 2) {
+          return res.redirect('/usuario');
+        } else {
+          return res.redirect('/homeadmin');
         }
-      })(req, res, next);
+      });
+    } catch (error) {
+      return next(new AppError(error, `/acceso`));
+    }
+  })(req, res, next);
 }
 
 exports.getRegistro = (req, res, next) => {
@@ -37,36 +37,36 @@ exports.getRegistro = (req, res, next) => {
 }
 
 exports.postRegistro = async (req, res, next) => {
-    try {
+  try {
 
-        let {
-            nombres, apellidos, correo, contrasena, confirmarContrasena
-          } = req.body;
+    let {
+      nombres, apellidos, correo, contrasena, confirmarContrasena
+    } = req.body;
 
-          correo = correo.trim().toLowerCase()
-          
-          if (contrasena != confirmarContrasena) throw new Error('Las contraseñas no coinciden')
-          const existingUser = await Usuario.findOne({ correo })
-          if (existingUser) throw new Error("Ya existe un usuario registrado con ese correo.")
+    correo = correo.trim().toLowerCase()
 
-          const tipoUsuario = await TipoUsuario.findOne({"nombre": "Usuario"})
-          
-          if (!tipoUsuario) throw new Error("No existe el tipo de usuario.")
+    if (contrasena != confirmarContrasena) throw new Error('Las contraseñas no coinciden')
+    const existingUser = await Usuario.findOne({ correo })
+    if (existingUser) throw new Error("Ya existe un usuario registrado con ese correo.")
 
-          const newUser = new Usuario({
-            nombres, apellidos, correo, contrasena, tipoUsuario: tipoUsuario._id
-          });
+    const tipoUsuario = await TipoUsuario.findOne({ "nombre": "Usuario" })
 
-          await newUser.save();
+    if (!tipoUsuario) throw new Error("No existe el tipo de usuario.")
 
-          req.logIn(newUser, (err) => {
-            if (err) return next(err);
-                return res.redirect('usuario');
-          });
+    const newUser = new Usuario({
+      nombres, apellidos, correo, contrasena, tipoUsuario: tipoUsuario._id
+    });
 
-    } catch (error) {
-        return next(new AppError(error, `/acceso/registro`));
-    }
+    await newUser.save();
+
+    req.logIn(newUser, (err) => {
+      if (err) return next(err);
+      return res.redirect('usuario');
+    });
+
+  } catch (error) {
+    return next(new AppError(error, `/acceso/registro`));
+  }
 }
 
 exports.getOlvido = (req, res, next) => {
@@ -74,4 +74,10 @@ exports.getOlvido = (req, res, next) => {
 }
 
 exports.postOlvido = async (req, res, next) => {
+}
+
+exports.logout = (req, res) => {
+  req.session.destroy(function (err) {
+    res.redirect('/');
+  });
 }
